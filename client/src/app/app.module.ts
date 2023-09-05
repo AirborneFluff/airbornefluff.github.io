b8658577ece2ea26458c4e75dfb9eb1154b62087
AirborneFluff <proffitt.m.98@gmail.com>
2023-09-06 00:00:21 +0100

    Sorted theming being stored as an object (hydrated), removed hydrate pipe

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -6,7 +6,7 @@ import { ToggleThemeComponent } from "./core/theming/toggle-theme/toggle-theme.c
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IsVisibleDirective } from './core/common/directives/is-visible.directive';
 import { ScrollIndicatorComponent } from './core/components/scroll-indicator/scroll-indicator.component';
-import {MatIconModule} from "@angular/material/icon";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
   declarations: [
