da94ade5dfbdc9d0e650965ad0ba33494aca9a06
AirborneFluff <proffitt.m.98@gmail.com>
2023-09-12 20:39:37 +0100

    Implementing dialog

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { IsVisibleDirective } from './core/common/directives/is-visible.directive';
 import { ScrollIndicatorComponent } from './core/components/scroll-indicator/scroll-indicator.component';
 import { MatIconModule } from "@angular/material/icon";
+import { TestModalComponent } from './features/test-modal/test-modal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     IsVisibleDirective,
     ScrollIndicatorComponent,
-    IsVisibleDirective
+    IsVisibleDirective,
+    TestModalComponent
   ],
   imports: [
     BrowserModule,
